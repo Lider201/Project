05803b4ebe777567048ef882f33896df88d4297c
Vladyslav <123257611+Lider201@users.noreply.github.com>
2025-04-08 00:19:00 +0300

    Add files via upload

--- My project/index.js
+++ My project/index.js
@@ -1,3 +1,23 @@
+const tracks = [
+    {
+        title: "OZEN",
+        artists: "TENZOO, Zhanbxqqm SKYVER",
+        image: "/public/images/image_example.jpg",
+        audio: "/public/trecks/example.mp3"
+    },
+    {
+        title: "Ransom",
+        artists: "Lil Tecca",
+        image: "/public/images/image_example_1.jpg",
+        audio: "/public/trecks/example_2.mp3"
+    },
+    {
+        title: "Lost Soul",
+        artists: "NBSPLV",
+        image: "/public/images/image_example_2.jpg",
+        audio: "/public/trecks/example_1.mp3"
+    }
+]
 function openTab(env, name) {
     let i, tabcontent, tablinks;
     tabcontent=document.getElementsByClassName("tabcontent");
@@ -10,4 +30,39 @@ function openTab(env, name) {
     }
     document.getElementById(name).style.display="block";
     env.currentTarget.className+=" active";
+}
+function renderPlayList(tracks) {
+    const container = document.getElementById("MyPlaylist");
+    container.innerHTML="";
+    tracks.forEach(track => {
+        const player = document.createElement("div");
+        player.classList.add("player", "small");
+        player.innerHTML = `
+        <img class="track-image" src="${track.image}" alt="Track Cover">
+        <div class="audio-controls">
+            <h3>${track.title}</h3>
+            <h5>${track.artists}</h5>
+            <audio controls>
+                <source src="${track.audio}" type="audio/mpeg">
+            </audio>
+        </div>
+        `;
+        container.appendChild(player);
+    });
+    setupAudioPauseBehaviour();
+}
+document.addEventListener("DOMContentLoaded", () => {
+    renderPlayList(tracks);
+})
+function setupAudioPauseBehaviour() {
+    const audioElements = document.querySelectorAll("audio");
+    audioElements.forEach(audio=> {
+    audio.addEventListener("play", () => {
+        audioElements.forEach(otherAudio => {
+            if (otherAudio !== audio) {
+                otherAudio.pause();
+            }
+        });
+    });
+});
 }
